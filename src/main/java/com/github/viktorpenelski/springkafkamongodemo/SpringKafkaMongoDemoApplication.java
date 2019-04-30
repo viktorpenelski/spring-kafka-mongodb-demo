5ac73bf3d10b0e47e2f7be1723fa6a222697cff6
viktorpenelski <victor.penelski@gmail.com>
2019-04-30 18:51:54 +0300

    Initial commit, initialized with SpringInitializr

---
+++ src/main/java/com/github/viktorpenelski/springkafkamongodemo/SpringKafkaMongoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.viktorpenelski.springkafkamongodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaMongoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaMongoDemoApplication.class, args);
+	}
+
+}
